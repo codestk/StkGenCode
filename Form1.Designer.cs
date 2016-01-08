cf03c29bf55574384f0c8171177c39b303614a80
momojojo <codestk@gmail.com>
2016-01-09 01:45:49 +0700

    innit

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace StkGenCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(399, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "c:\\Code\\";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(399, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Data Source=NODE-PC;Initial Catalog=WEBAPP;User ID=sa;Password=P@ssw0rd";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(578, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "GenFile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ConnectionString";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.ColumnWidth = 150;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(61, 84);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(494, 394);
+            this.checkedListBox1.Sorted = true;
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 488);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StkGenCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //List All Table to display
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql;
+            sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'";
+            SqlConnection _FbConnection = new SqlConnection();
+            _FbConnection.ConnectionString = textBox2.Text;
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, _FbConnection);
+            adapter.Fill(ds);
+            //checkedListBox1.DataSource = ds.Tables[0];
+
+            foreach (DataRow item in ds.Tables[0].Rows)
+            {
+                checkedListBox1.Items.Add(item["TABLE_NAME"].ToString(), false);
+            }
+        }
+
+        //Write file by path
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string codeAfterGen = "dsgmdkl kfjkj ekjrefjriefjirjf er irj g";
+
+            ClearAllFile(textBox1.Text);
+            GenAspx("LogX");
+        }
+
+        //Delete All Find in folder
+        private void ClearAllFile(string patch)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(patch);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+
+            // File.Delete(path);
+        }
+
+        #region apsxCode
+
+        //Gen Aspx File
+        private void GenAspx(string Table)
+        {
+            string aspxHtml;
+            //Begin Content===================================================
+            aspxHtml = "<asp:Content ID='Content1' ContentPlaceHolderID='HeadContent' runat='server'>";
+            writeFile(Table, aspxHtml);
+
+            //style -----------------------------------------------------------
+            aspxHtml = "<style type='text/css'>";
+            writeFile(Table, aspxHtml);
+            AddSpace(Table);
+            aspxHtml = "</style>";
+            writeFile(Table, aspxHtml);
+            AddSpace(Table);
+            //-----------------------------------------------------------------
+
+            //Script ----------------------------------------------------------
+            aspxHtml = " <script type='text/javascript'>";
+            writeFile(Table, aspxHtml);
+
+            aspxHtml = "$(document).ready(function() {";
+            writeFile(Table, aspxHtml);
+
+            AddSpace(Table);
+
+            aspxHtml = "});";
+            writeFile(Table, aspxHtml);
+            aspxHtml = "</script>";
+            writeFile(Table, aspxHtml);
+            //-----------------------------------------------------------------
+
+            aspxHtml = @"</asp:Content>";
+            writeFile(Table, aspxHtml);
+            AddSpace(Table);
+            //======================================================End Content
+
+            //Begin Content====================================================
+            aspxHtml = @"<asp:Content ID='Content2' ContentPlaceHolderID='MainContent' runat='server'>";
+            writeFile(Table, aspxHtml);
+            AddSpace(Table);
+
+            aspxHtml = "< asp:Panel ID ='pnlSave' runat='server'>";
+            AddSpace(Table);
+            aspxHtml = @"</asp:Content>";
+            writeFile(Table, aspxHtml);
+            AddSpace(Table);
+            //======================================================End Content
+        }
+
+        private void AddSpace(string Table)
+        {
+            string space = "\r\n";
+            writeFile(Table, space);
+        }
+
+        #endregion apsxCode
+
+        public void writeFile(string Table, string content)
+        {
+            string path = textBox1.Text;
+            // check if directory exists
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            path = path + Table + "_" + DateTime.Today.ToString("dd-MM-yy") + ".aspx";
+            // check if file exist
+            //File.Delete(path);
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+            }
+
+            // log the error now
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                string error = content;
+                writer.WriteLine(error);
+                //writer.WriteLine("==========================================");
+                writer.Flush();
+                writer.Close();
+            }
+            //  return userFriendlyError;
+        }
+    }
+}
